d00bfe3115cdde5becde05a21154dfa87192f822
tu11aa <thanhtuhr01@gmail.com>
2023-02-25 10:51:07 +0700

    success

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 string keyGenerate(size_t size)
 {
-    srand(time(NULL));
     string rs = "";
     const int TOCHAR = 97;
     for (int i = 0; i < size; i++)
@@ -12,14 +11,11 @@ string keyGenerate(size_t size)
     return rs;
 }
 
-// string vernamEncryptvme(string ciphertext, string key)
+// string vernamEncrypt(string ciphertext, string key)
 // {
 //     string rs = "";
 //     for (size_t i = 0; i < ciphertext.length(); i++)
-//     {
-//         rs += char(((ciphertext[i] - 97) ^ (key[i] - 97)) % 26 + 97);
-//         cout << ((ciphertext[i] - 97) ^ (key[i] - 97)) % 26 + 97 << ' ';
-//     }
+//         rs += char(((ciphertext[i] - 97) ^ (key[i] - 97)) + 97);
 //     return rs;
 // }
 
@@ -28,7 +24,7 @@ string vernamEncrypt(string plaintext, string key)
     string message = "";
     for (int i = 0; i < plaintext.length(); i++)
     {
-        message += (char)((((int)plaintext[i] - 97) ^ ((int)(key[i] - 97))) % 26 + 97);
+        message += (char)((((int)plaintext[i] - 97) ^ ((int)(key[i] - 97))) + 97);
     }
     return message;
 }
@@ -37,21 +33,22 @@ string vernamDecrypt(string ciphertext, string key)
 {
     string rs = "";
     for (size_t i = 0; i < ciphertext.length(); i++)
-    {
-        rs += char(((ciphertext[i] - 97) ^ (key[i] - 97)) % 26 + 97);
-        // cout << ((ciphertext[i] - 97) ^ (key[i] - 97)) % 26 + 97 << ' ';
-    }
+        rs += char(((ciphertext[i] - 97) ^ (key[i] - 97)) + 97);
     return rs;
 }
 
 int main()
 {
+    srand(time(NULL));
     string p = "helloworld", c = "", k = "";
-    k = keyGenerate(p.length());
-    cout << "key: " << k << endl;
-    c = vernamEncrypt(p, k);
-    cout << "c: " << c << endl;
-    string new_p = vernamDecrypt(c, k);
-    cout << "new p: " << new_p << endl;
-    return 0;
+
+    for (int i = 0; i < 20; i++)
+    {
+        k = keyGenerate(p.length());
+        cout << "Key : " << k << endl;
+        c = vernamEncrypt(p, k);
+        cout << "Ciphertext : " << c << endl;
+        cout << "plaintext : " << vernamDecrypt(c, k) << endl
+             << endl;
+    }
 }
