0784d1081d7c9f0160289a3f62d80a22a1de3a98
dqbinh02 <dqbinh20@clc.fitus.edu.vn>
2023-03-02 06:10:01 +0700

    update code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 string keyGenerate(size_t size)
@@ -11,20 +10,12 @@ string keyGenerate(size_t size)
     return rs;
 }
 
-// string vernamEncrypt(string ciphertext, string key)
-// {
-//     string rs = "";
-//     for (size_t i = 0; i < ciphertext.length(); i++)
-//         rs += char(((ciphertext[i] - 97) ^ (key[i] - 97)) + 97);
-//     return rs;
-// }
-
 string vernamEncrypt(string plaintext, string key)
 {
     string message = "";
     for (int i = 0; i < plaintext.length(); i++)
     {
-        message += (char)((((int)plaintext[i] - 97) ^ ((int)(key[i] - 97))) + 97);
+        message += ((plaintext[i]-'a') ^ (key[i]-'a')) + 'a';
     }
     return message;
 }
@@ -33,14 +24,14 @@ string vernamDecrypt(string ciphertext, string key)
 {
     string rs = "";
     for (size_t i = 0; i < ciphertext.length(); i++)
-        rs += char(((ciphertext[i] - 97) ^ (key[i] - 97)) + 97);
+        rs += ((ciphertext[i]-'a') ^ (key[i]-'a')) + 'a';
     return rs;
 }
 
 int main()
 {
     srand(time(NULL));
-    string p = "helloworld", c = "", k = "";
+    string p = "duongquangbinh", c = "", k = "";
 
     for (int i = 0; i < 20; i++)
     {
@@ -48,7 +39,8 @@ int main()
         cout << "Key : " << k << endl;
         c = vernamEncrypt(p, k);
         cout << "Ciphertext : " << c << endl;
-        cout << "plaintext : " << vernamDecrypt(c, k) << endl
-             << endl;
+        cout << "plaintext : " << vernamDecrypt(c, k) << endl;
+        cout << endl;
     }
+    return 0;
 }
